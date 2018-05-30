2065ec635c27ebcf1f392b39133cdc3aaf4e2a03
haqueo <omarhaque@hotmail.co.uk>
2018-05-30 20:47:09 +0100

    more refactoring, added function docs

--- srcf/searchTools/fullSearch.cpp
+++ srcf/searchTools/fullSearch.cpp
@@ -1,10 +1,9 @@
 //============================================================================
 // Name        : fullSearch.cpp
-// Author      : Omar Haque, but almost all of the entropy code is by Patrick E.
-// 				 Meyer found in the infotheo package.
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Omar Haque
+// Version     : 1.0
+// Copyright   : MIT
+// Description : The tools used to compute the search
 //============================================================================
 
 #include <iostream>
@@ -22,67 +21,65 @@ using namespace std;
 #include <math.h>
 #include "../entropy/entropy.h"
 
+/**
+    Returns an array of information theoretic measures
 
+    @param p1 The index of the first site
+    @param Hp1 The entropy of the first site
+    @param p2 The index of the second site
+    @param Hp2 The entropy of the second site
+    @param p3 The index of the third site
+    @param cl The index of the class
+    @param p2 The index of the second site
+    @param p3 The index of the third site
+    @param *d Pointer to the first element of the data
+    @param nsamples The number of samples (rows)
+    @param nvars The number of variables (columns) including Class
+    @param Hcl The entropy of the class column
+    @param Hp1cl The joint entropy of the first site and Class column
 
-
+    @return Array containing [IGStrict,IGAlt,IGNew,VIp,VIc]
+*/
 double *calculateMeasures(int p1, double Hp1, int p2, double Hp2, int p3, int cl, const int *d,
 		int nsamples, int nvars, int c,double Hcl, double Hp1cl){
 	static double final[5];
 	int selTempNew[4] = {-1,-1,-1,-1};
 
-	////// calculation of base entropies and joint entropies
-
-	// H(P1)
-	// this is entropyp1
-
-	// H(P1,C)
-	// this is entropyp1cl
-
-	// H(P2)
-	// this is entropyp2
-
+	/*** calculation of base entropies and joint entropies ***/
 	// H(P2,C)
 	selTempNew[0] = p2;
 	selTempNew[1] = cl;
 	double Hp2cl = entropyFast(d,nsamples,nvars,0,selTempNew);
 
 	// H(P3,C)
-
 	selTempNew[0] = p3;
 	double Hp3cl = entropyFast(d,nsamples,nvars,0,selTempNew);
 
 	// H(P3)
-
 	selTempNew[1] = -1;
 	double Hp3 = entropyFast(d,nsamples, nvars,0,selTempNew);
 
 	// H(P1,P2)
-
 	selTempNew[0] = p1;
 	selTempNew[1] = p2;
 	double Hp1p2 = entropyFast(d,nsamples, nvars,0,selTempNew);
 
 	//H(P1,P2,C)
-
 	selTempNew[2] = cl;
 	double Hp1p2cl = entropyFast(d,nsamples,nvars,0,selTempNew);
 
 	// H(P1,P3)
-
 	selTempNew[2] = -1;
 	selTempNew[1] = p3;
-
 	double Hp1p3 = entropyFast(d,nsamples,nvars,0,selTempNew);
 
 	// H(P1,P3,C)
-
 	selTempNew[2] = cl;
 	double Hp1p3cl = entropyFast(d,nsamples,nvars,0,selTempNew);
 
 	// H(P2,P3)
 	selTempNew[0] = p2;
 	selTempNew[2] = -1;
-
 	double Hp2p3 = entropyFast(d,nsamples,nvars,0,selTempNew);
 
 	// H(P2,P3,cl)
@@ -91,7 +88,6 @@ double *calculateMeasures(int p1, double Hp1, int p2, double Hp2, int p3, int cl
 
 	// H(P1,P2,P3)
 	selTempNew[2] = p1;
-
 	double Hp1p2p3 = entropyFast(d,nsamples,nvars,0,selTempNew);
 
 	// H(P1,P2,P3,C)
@@ -118,7 +114,7 @@ double *calculateMeasures(int p1, double Hp1, int p2, double Hp2, int p3, int cl
 	double VIp2cl = 2*Hp2cl - Hp2 - Hcl;
 	double VIp3cl = 2*Hp3cl - Hp3 - Hcl;
 
-
+	// calculation of the measures
 	double IG_strict = Ip1p2p3 - max(IGp1p2,0.0) - max(IGp1p3,0.0) - max(IGp2p3,0.0) - Ip1 - Ip2 - Ip3;
 	double IG_alt = Ip1p2p3 - IGp1p2 - IGp1p3 - IGp2p3 - Ip1 - Ip2 - Ip3;
 	double IG_new = Ip1p2p3;
@@ -134,42 +130,48 @@ double *calculateMeasures(int p1, double Hp1, int p2, double Hp2, int p3, int cl
 }
 
 
-
+/**
+ * Reads in a genetic data file of the form nrows x nvar
+ *
+ * @param filename The File to be read in
+ * @param nrows The number of rows in the .txt
+ * @param nvars The number of columns in the.txt
+ *
+ * @return Pointer to the data in a 1D format, column first.
+ */
 std::vector<int> readData(std::string filename, int nrows, int nvars){
-	/*
-	 * Format data should be: nrows x nvars csv of integers 0,1 (minor or major alleles)
-	 * the Class type is the last of these column included in nvars. So its index is
-	 * nvars-1, i.e. the last column.
-	 *
-	 *
-	 * Output: the data in a 1d std::vector, arranged by columns first. So c1^T, c2^T,...,cN^T
-	 */
 
 	const int datasize = nvars*nrows;
 	static std::vector<int> data(datasize);
 	static std::vector<int> dataReal(datasize);
 
     std::fstream myfile(filename.c_str(), std::ios_base::in);
-
     int a;
 
+    // read in the data as is.
     for(int i =0; i< datasize;i++){
     	myfile >> a;
     	data[i] = a;
     }
 
+    // need to transform the data so it's column first.
     for(int s = 0; s < nrows; s++){
     	for (int i = 0; i<nvars;i++){
     		dataReal[s+i*nrows] = data[i+s*nvars];
     	}
-
     }
-
-    // this reads in the data as is. I actually need to transpose it.
 	return dataReal;
 
 }
 
+/**
+ * Read in the n'th index pair from indexes.txt
+ *
+ * @param filename The location of indexes.txt
+ * @param n How many lines down to get indices from
+ *
+ * @return The n'th line from indexes.txt
+ */
 std::vector<int> readIndices(std::string filename, int n){
 
 	std::ifstream infile(filename.c_str());
@@ -180,26 +182,31 @@ std::vector<int> readIndices(std::string filename, int n){
    for(int i = 0; i < n; ++i){
 	   std::getline(infile, s);
    }
-   // s is now the n'th line
 
    std::replace( s.begin(), s.end(), ',', ' ' );
-
    stringstream ss(s);
-
    copy(istream_iterator<int>(ss), istream_iterator<int>(), back_inserter(indexes));
 
    return indexes;
 }
-
+/**
+ * Run through all of the triples of the dataset, calculating measures for each triple.
+ *
+ * @param filename The filename for the genetic dataset
+ * @param outputFilename The filename for the printed measures to be outputted
+ * @param nsamples Number of samples in the genetic dataset (number of rows)
+ * @param nvars Number of variables in the genetic dataset (number of columns) including Class
+ * @param c The estimator to use, 0 uses empirical entropy. See entropy.cpp for details
+ * @param printall Indicates whether to print measures for every triple
+ * @param assocLevel If printall is false, measures are printed if IGStrict > assocLevel*H(CL)
+ */
 void runFullSearch(std::string filename, std::string outputFilename, int nsamples, int nvars,
 		int c, bool printall, double assocLevel){
+
 	std::vector<int> d = readData(filename, nsamples, nvars);
-
 	const int* p = d.data();
-
 	int v[4] = {-1,-1,-1,-1};
 	v[0] = nvars-1;
-
 	double Hcl = entropyFast(p,nsamples,nvars,c,v);
 	v[0] = -1;
 
@@ -214,7 +221,6 @@ void runFullSearch(std::string filename, std::string outputFilename, int nsample
 	double measure4sum = 0.0;
 	double measure4squaredsum = 0.0;
 	int iterations = 0;
-
 
 
 	ofstream myfile (outputFilename.c_str());
@@ -270,34 +276,46 @@ void runFullSearch(std::string filename, std::string outputFilename, int nsample
 	} else {
 		cout << "Unable to open file";
 	}
-
-
-
 	ofstream myfile2 ("means.txt");
 
 	if (myfile2.is_open()){
 		myfile2 << "averageIGstrict: " << measure0sum/iterations << std::endl;
-		myfile2 << "stddevIGstrict: " << sqrt(measure0squaredsum/iterations - (pow(measure0sum/iterations,2))) << std::endl;
+		myfile2 << "avgIGstrictSquared: " << measure0squaredsum/iterations << std::endl;
 
 		myfile2 << "averageIGalt: " << measure1sum/iterations << std::endl;
-		myfile2 << "stddevIGalt: " << sqrt(measure1squaredsum/iterations - (pow(measure1sum/iterations,2))) << std::endl;
+		myfile2 << "avgIGaltSquared: " << measure1squaredsum/iterations << std::endl;
 
 		myfile2 << "averageVIp: " << measure3sum/iterations << std::endl;
-		myfile2 << "stddevVIp: " << sqrt(measure3squaredsum/iterations - (pow(measure3sum/iterations,2))) << std::endl;
+		myfile2 << "avgVIpSquared: " << measure3squaredsum/iterations << std::endl;
 
 
 		myfile2 << "averageVIc: " << measure4sum/iterations << std::endl;
-		myfile2 << "stddevVIc: " << sqrt(measure4squaredsum/iterations - (pow(measure4sum/iterations,2))) << std::endl;
+		myfile2 << "avgVIcSquared: " <<measure4squaredsum/iterations  << std::endl;
 
 		myfile2 << "iterations:" << iterations << std::endl;
 
 		myfile2.close();
 	}
-
-
-
 }
 
+
+/**
+ * Run through specified indexes in the dataset, calculating measures for each triple.
+ *
+ * @param filename The filename for the genetic dataset
+ * @param outputFilename The filename for the printed measures to be outputted
+ * @param nsamples Number of samples in the genetic dataset (number of rows)
+ * @param nvars Number of variables in the genetic dataset (number of columns) including Class
+ * @param c The estimator to use, 0 uses empirical entropy. See entropy.cpp for details
+ * @param istart The index i to start at, i.e. (istart,jstart,kstart) - inclusive
+ * @param iend The index i to end at, i.e. (iend,jend,kend) - inclusive
+ * @param jstart The index j to start at, i.e. (istart,jstart,kstart) - inclusive
+ * @param jend The index j to end at, i.e. (iend,jend,kend) - inclusive
+ * @param kstart The index k to start at, i.e. (istart,jstart,kstart) - inclusive
+ * @param kend The index k to end at, i.e. (iend,jend,kend) - inclusive
+ * @param printall Indicates whether to print measures for every triple
+ * @param assocLevel If printall is false, measures are printed if IGStrict > assocLevel*H(CL)
+ */
 void runFullSearchIndexes(std::string filename, std::string outputFilename, int nsamples, int nvars,
 		int c, int istart, int iend, int jstart, int jend, int kstart, int kend,
 		bool printall, double assocLevel){
@@ -353,7 +371,6 @@ void runFullSearchIndexes(std::string filename, std::string outputFilename, int 
 			jstartreal = i+1;
 		}
 
-
 		for (int j = jstartreal; (j < nvars-2) && !Done; j++){
 
 			v[0] = j;
@@ -401,32 +418,26 @@ void runFullSearchIndexes(std::string filename, std::string outputFilename, int 
 	} else {
 		cout << "Unable to open file";
 	}
-
-
 	ofstream myfile2 ("means.txt");
 
 	if (myfile2.is_open()){
+
 		myfile2 << "averageIGstrict: " << measure0sum/iterations << std::endl;
-				myfile2 << "stddevIGstrict: " << sqrt(measure0squaredsum/iterations - (pow(measure0sum/iterations,2))) << std::endl;
+		myfile2 << "avgIGstrictSquared: " << measure0squaredsum/iterations << std::endl;
 
-				myfile2 << "averageIGalt: " << measure1sum/iterations << std::endl;
-				myfile2 << "stddevIGalt: " << sqrt(measure1squaredsum/iterations - (pow(measure1sum/iterations,2))) << std::endl;
+		myfile2 << "averageIGalt: " << measure1sum/iterations << std::endl;
+		myfile2 << "avgIGaltSquared: " << measure1squaredsum/iterations << std::endl;
 
-				myfile2 << "averageVIp: " << measure3sum/iterations << std::endl;
-				myfile2 << "stddevVIp: " << sqrt(measure3squaredsum/iterations - (pow(measure3sum/iterations,2))) << std::endl;
-
-
-				myfile2 << "averageVIc: " << measure4sum/iterations << std::endl;
-				myfile2 << "stddevVIc: " << sqrt(measure4squaredsum/iterations - (pow(measure4sum/iterations,2))) << std::endl;
+		myfile2 << "averageVIp: " << measure3sum/iterations << std::endl;
+		myfile2 << "avgVIpSquared: " << measure3squaredsum/iterations << std::endl;
 
 
+		myfile2 << "averageVIc: " << measure4sum/iterations << std::endl;
+		myfile2 << "avgVIcSquared: " <<measure4squaredsum/iterations  << std::endl;
+
+		myfile2 << "iterations:" << iterations << std::endl;
 
 			myfile2.close();
 		}
-
-
-
-
-
 }
 
